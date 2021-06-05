becc95c056aad3e1237b6d87fc792771aa5bdaa8
bdnaimur <webnaimur@gmail.com>
2021-06-05 14:16:53 +0600

    blog

--- js/main.js
+++ js/main.js
@@ -17,4 +17,23 @@ $(document).ready(function(){
             }
         }
     })
+
+    $('.chioce-carousel').owlCarousel({
+        // stagePadding: 80,
+        loop:true,
+        margin:15,
+        nav:true,
+        navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1400:{
+                items:3
+            }
+        }
+    })
   });
